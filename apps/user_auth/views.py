d9ea0b123a79d33f6ebdc149c5ecf507624a9f5f
den <denniskitun@gmail.com>
2017-12-21 01:53:32 +0200

    added registration and authorization

--- apps/user_auth/views.py
+++ apps/user_auth/views.py
@@ -1,4 +1,21 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from .forms import UserCreation
+from .models import User
+from django import forms
 def hello(request):
     return render(request, 'user_auth/index.html')
+
+
+class SignUp(CreateView):
+    template_name = "registration/register.html"
+    model = User
+    form_class = UserCreation
+    success_url = "/"
+
+
+class SignIn(LoginView):
+    template_name = 'registration/login.html'
+
--- social_proj/urls.py
+++ social_proj/urls.py
@@ -4,8 +4,13 @@ The `urlpatterns` list routes URLs to views.
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from apps.user_auth import views as proj_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin', admin.site.urls),
+    url(r'^account/login/',  proj_views.SignIn.as_view(), name='login'),
+    url(r'^account/logup/', proj_views.SignUp.as_view(), name='register'),
+    url(r'^account/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('apps.user_auth.urls')),
 ]
